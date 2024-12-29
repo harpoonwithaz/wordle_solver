f864001dff2c5a23ebc4b8b42ecc73a4c1132698
harpoonwithaz <harpoongamingzr@gmail.com>
2024-12-28 23:34:16 -0800

    added feedback, simplified wordle game
    Created feedback module and simplified the wordle game

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 A Python project to create an algorithm that solves Wordle in the most efficient way possible. Collaboratively developed using GitHub.
 
-## Roadmap
+## Planning roadmap
 
+- [x] Wordle Game
+- [X] Export letter types
+- [X] Filter words from data
+- [ ] Make algorithm for choosing words
+    - [ ] Using the feedback from main.py it will return a guess
+    - [ ] Impliment algorithm to guess
---
+++ src/main.py
@@ -0,0 +1,44 @@
+import random, os
+import feedback
+
+# Reads words.txt in local directory and 
+word_list = (open(f'{os.getcwd()}\\data\\words.txt','r')).read().splitlines()
+
+secret_word = random.choice(word_list)
+
+attempts = 6
+
+while attempts > 0:
+    while True:
+        # Asks user for guess
+        user_guess = input("Enter your 5-letter guess: ").strip().lower()
+
+
+        '''
+        
+        HERE WE WILL IMPLIMENT THE ALGORITHM.PY MODULE FOR MAKING THE GUESS USING FEEDBACK
+        
+        '''
+
+        # Checks if it has 5 characters
+        if len(user_guess) == 5 and user_guess.isalpha():
+            break
+            print("Invalid input. Please enter a 5-letter word.")
+
+        # Checks if guess is a valid word
+        if user_guess not in word_list:
+            print('Invalid word')
+
+    if user_guess == secret_word:
+        print(f'Victory, the word was: {secret_word}')
+
+    feedback = feedback.get_feedback(user_guess, secret_word, word_list)
+
+    print(f"Feedback: {feedback}")
+    attempts -= 1
+    if attempts > 0:
+        print(f"You have {attempts} attempts remaining.\n")
+    else:
+        print(f"Game over! The secret word was: {secret_word}")
+
+    print(f'Secret word btw: {secret_word}')
