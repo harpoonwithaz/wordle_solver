21f135bd41afe6be118956d7388b7b2272c82b15
Me3e3ep <jaydennguyenphan@gmail.com>
2024-12-31 10:44:34 -0800

    fixed feedback
    If the user inputs "angla" and the secret is "aback" the feedback output will be "GXXXY". if the user still inputs "angla" and the secret is "abcck" (so there is only one 'a') the feedback will be "GXXXX" to correctly give feedback that there is only one 'a'

--- src/feedback.py
+++ src/feedback.py
@@ -15,5 +15,13 @@ def get_feedback(guess : str, secret : str, word_list : list) -> list:
             feedback.append('Y')  # Yellow: correct letter but wrong position
         else:
             feedback.append('X')  # Gray: letter not in the word
+    for j in range(5):
+        if secret.count(guess[j]) == feedback.count("G"):
+            for user_word_index in range(5):
+                if feedback[user_word_index] == "Y": 
+                    feedback[user_word_index] = "X"
+                    
     return ''.join(feedback)
 
+#answer = get_feedback("angla","aback",[]) #for testing
+#print(answer)
