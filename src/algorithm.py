031a5f746a98d75d5e5f6f63f95f8c7815eb7d80
Me3e3ep <jaydennguyenphan@gmail.com>
2024-12-30 21:44:34 -0800

    Added to algorithm
    i added part of algorithm, It can guess what word to guess. I just need to implement the removing words from the list part

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,3 +1,39 @@
+#Jayden Phan - program to see the amount of letters in a list of words, and get the word with the most frequent letters in the list
 
 
-def create_guess(feedback):
+def get_key_from_value(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None  # If the value is not found
+
+
+def create_guess(word_list):
+    alphabet_dict = {}
+    for word in word_list:
+        for letter in word:
+            alphabet_dict[letter]["count"] += 1
+    ''' the code below is not needed. but it takes the amount of occurrences of a letter then divides it by the total amount of letters. This will get the letter as a percentage of the total letters.
+    for letter in alphabet_dict:
+        alphabet_dict[letter]["weight"] = round(((alphabet_dict[letter]["count"]/totalcounter)*100),3)
+    '''
+    #word_value = {lister[x]:0 for x in range(len(lister))}
+    word_value = {}
+
+    for word in word_list:
+        unique_letters = set(word)
+        score = sum(alphabet_dict[letter]["count"] for letter in unique_letters)
+        word_value[word] = score
+
+    answer = max(word_value, key=word_value.get)
+    print(answer)
+
+
+'''     
+for key, value in word_value.items():
+    print(f"{key}: {value}") 
+
+for key, value in alphabet_dict.items():
+    print(f"{key}: {value}") 
+''' 
+
