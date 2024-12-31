67015a5112a009808b67fd9849a4c74089edf679
harpoonwithaz <harpoongamingzr@gmail.com>
2024-12-30 23:36:02 -0800

    added exit feature

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import random, os
 import feedback
 
 # Reads words.txt in local directory and 
-word_list = (open(f'{os.getcwd()}\\wordle_solvr\\wordle_solver\\data\\words.txt','r')).read().splitlines() #this is for jayden, comment this line and use the other one
+word_list = (open(f'{os.getcwd()}\\data\\words.txt','r')).read().splitlines() #this is for jayden, comment this line and use the other one
 #word_list = (open(f'{os.chdir("\\data\\words.txt")}','r')).read().splitlines()
 #secret_word = random.choice(word_list)
 secret_word = "angle"
@@ -21,25 +21,31 @@ while attempts > 0:
         
         '''
 
+        if user_guess == 'exit':
+            break
+        
         # Checks if it has 5 characters
         if len(user_guess) == 5 and user_guess.isalpha():
-            break
             print("Invalid input. Please enter a 5-letter word.")
+            break
 
         # Checks if guess is a valid word
         if user_guess not in word_list:
             print('Invalid word')
 
-    if user_guess == secret_word:
-        print(f'Victory, the word was: {secret_word}')
-
-    feedback = feedback.get_feedback(user_guess, secret_word, word_list)
-
-    print(f"Feedback: {feedback}")
-    attempts -= 1
-    if attempts > 0:
-        print(f"You have {attempts} attempts remaining.\n")
+    if user_guess == 'exit':
+        break
     else:
-        print(f"Game over! The secret word was: {secret_word}")
+        if user_guess == secret_word:
+            print(f'Victory, the word was: {secret_word}')
 
-    print(f'Secret word btw: {secret_word}')
+        feedback = feedback.get_feedback(user_guess, secret_word, word_list)
+
+        print(f"Feedback: {feedback}")
+        attempts -= 1
+        if attempts > 0:
+            print(f"You have {attempts} attempts remaining.\n")
+        else:
+            print(f"Game over! The secret word was: {secret_word}")
+
+        print(f'Secret word btw: {secret_word}')
