cfe40e38b8ae3f8c8ee1205f930dcde9f5afe526
harpoonwithaz <otad19@gmail.com>
2025-01-02 18:38:40 -0800

    added discord application core features
    Added discord bot's features using discord.py library

---
+++ src/discord_bot/discord_bot.py
@@ -0,0 +1,51 @@
+import os
+from dotenv import load_dotenv
+
+# Discord bot imports
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+# load token from safe file
+load_dotenv()
+bot_token = os.getenv('DISCORD_TOKEN')
+
+# Loads bot with intents
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = '%', intents = discord.Intents.default())
+
+# Attempts to sync commands on start
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is now running')
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} command(s)')
+    except Exception as e:
+        print(e)
+
+# Command to sync all the commands
+# THIS COMMAND DOESN'T WORK
+@bot.tree.command(name='sync')
+async def sync(interaction: discord.Integration):
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} command(s)')
+        await interaction.response.send_message(f'Successfully synced {len(synced)} command(s)')
+    except Exception as e:
+        print(e)
+        await interaction.response.send_message(f'Sync failed: {e}')
+
+# Testing command
+@bot.tree.command(name='test')
+async def hello(interaction: discord.Integration):
+    await interaction.response.send_message(f'Hello {interaction.user.mention}')
+
+# Wordle game command
+@bot.tree.command(name='play')
+async def play(interaction: discord.Integration):
+    
+
+# Runs bot with token from .env file
+bot.run(bot_token)
